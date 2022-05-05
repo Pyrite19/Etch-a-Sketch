ea80caefc05301db94203ae9018e858275445cf7
Timothy Davidson <timd1919@gmail.com>
2022-05-05 16:30:54 -0500

    Basic project finished.

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-const container = document.querySelector('.container');
-const box = document.querySelector('.box');
+const container = document.querySelector(".container");
+const box = document.querySelector(".box");
+const btn = document.querySelector(".btn");
+var c = 0
+var rowNum;
+var columnNum;
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.style.width = "32px"
-    div.style.height = "32px"
-    div.addEventListener("mouseover", (e) => {
-        div.style.backgroundColor = "black";
-    });
-    container.appendChild(div);
+btn.addEventListener("click", () => {
+	clearGrid();
+	rowNum = Number(prompt("Enter grid size"));
+    columnNum = rowNum
+	grid(rowNum, columnNum);
+});
+
+function grid(row, column) {
+	for (let c = 0, r = 0; c < column; r++) {
+		if (r < row) {
+			var col = document.querySelector(".col" + `${c}`);
+			const div = document.createElement("div");
+			div.classList.add("box");
+			div.addEventListener("mouseover", (e) => {
+				div.style.backgroundColor = "black";
+			});
+			col.appendChild(div);
+			console.log(`r = ${r}`);
+		} else {
+			r = -1;
+			c += 1;
+			newCol = document.createElement("div");
+			newCol.classList.add("col");
+			newCol.classList.add("col" + `${c}`);
+			container.appendChild(newCol);
+			console.log(`c = ${c}`);
+		}
+	}
+}
+
+function clearGrid() {
+	container.innerHTML = "";
+	newCol = document.createElement("div");
+	newCol.classList.add("col");
+	newCol.classList.add("col" + `${c}`);
+	container.appendChild(newCol);
 }
